45fbce3373c7e63c46877307b65cf4bc44bcb0df
Ale Brusco <alejandrobi89@gmail.com>
2016-06-02 19:43:14 -0300

    Corregido el comparar.sh

--- test/pruebas/comparar.sh
+++ test/pruebas/comparar.sh
@@ -2,9 +2,9 @@
 
 PRINCIPIO_NOMBRE_SALIDA=o # EJEMPLO: Si los archivos de salida son: o1.bpmn, o2.bpmn, ...; poner "o".
 
-for j in p1 p2 p3 ; do
+for j in p1 p2 p3 p4 p5 p6 ; do
 	UBICACION=$j
-	for i in $(ls $UBICACION/r*.bpmn | cut -d/ -f2 | cut -d. -f1 | cut -dr -f2) ; do
+	for i in $(ls $UBICACION/r*.bpmn | cut -d/ -f2 | cut -d. -f1 | cut -dr -f2 | sort -n) ; do
 		clear
 		echo "--- DIFF $i ---"
 		diff -ywB --suppress-common-lines $UBICACION/r$i.bpmn $UBICACION/$PRINCIPIO_NOMBRE_SALIDA$i.bpmn
